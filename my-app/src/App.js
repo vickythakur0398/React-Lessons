b41267096c4d343ee8be930da7bdb643050ddccb
vickythakur0398 <vickythakur0398@gmail.com>
2022-07-25 18:27:55 +0530

    adding more div and root elements concept

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,12 +21,12 @@ function App() {
     // </div>
 
     <div>
-    <h2>lets get started</h2>
-    <p>this is updating Tada</p>
+      <h2>lets get started</h2>
+      <p>this is updating Tada</p>
 
-    // SO HERE WE have used it just like html tags and than it will reflect on webpage
-    // this will tell react that we want this component to run as it alrerady imported at line 1
-    <ExpenseItem></ExpenseItem>
+      {/* SO HERE WE have used it just like html tags and than it will reflect on webpage*/
+      /*this will tell react that we want this component to run as it alrerady imported at line 1 */}
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
-//s1 here i have created a function i.e a component and it is a practice to sraert with Caps 
+//s1 here i have created a function i.e a component and it is a practice to sraert with Caps
 // bcs when we will import it into main js than we can make it tags like <h1></h1> simmilarly like <ExpenseItem>
 // </ExpenseItem> so page will know it is not html tags as it is not starting with small case
 
-function ExpenseItem()
-{
-        return <h2>Expense Item</h2>
-}
+function ExpenseItem() {
+  //   return <h2>Expense Item</h2>;
 
+  //here in retuirn statement we can only have one root elements so what if we have to add 3 more things
+  // return  <div>Amount</div>Title<div></div>Expense<div></div> so we will wrap it under one div i.e rot elements for now
+  return (
+    <div>
+      <div>July 25 </div>
+      <div>
+        <h2>Udemy Course</h2>
+      </div>
+      <div>499</div>
+    </div>
+  );
+}
 
 export default ExpenseItem;
