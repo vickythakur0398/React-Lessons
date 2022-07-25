05b39a1244ce43d47536f93c8283a707f143579d
vickythakur0398 <vickythakur0398@gmail.com>
2022-07-25 18:58:28 +0530

    added css

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -2,18 +2,24 @@
 // bcs when we will import it into main js than we can make it tags like <h1></h1> simmilarly like <ExpenseItem>
 // </ExpenseItem> so page will know it is not html tags as it is not starting with small case
 
+
+//2 now i am importing css style sheet which i have copied 
+
+import './ExpenseItem.css'
 function ExpenseItem() {
   //   return <h2>Expense Item</h2>;
 
   //here in retuirn statement we can only have one root elements so what if we have to add 3 more things
   // return  <div>Amount</div>Title<div></div>Expense<div></div> so we will wrap it under one div i.e rot elements for now
   return (
-    <div>
-      <div>July 25 </div>
-      <div>
+    <div className="expense-item">
+
+    {/*here since it looks like html but it is jsx and class is already reserved so to add css style we will use className*/}
+      <div className= "expense-item">July 25 2022 </div>
+      <div  className= "expense-item__description">
         <h2>Udemy Course</h2>
       </div>
-      <div>499</div>
+      <div className="expense-item__price">499</div>
     </div>
   );
 }
