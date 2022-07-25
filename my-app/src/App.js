fb02a536eac4ccff382c84185848bd79d169630e
vickythakur0398 <vickythakur0398@gmail.com>
2022-07-25 17:37:02 +0530

    added create react app to preview the server just installed all of this

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <div>
+    //     <h2>lets get started</h2></div>
+    //   </header>
+    // </div>
+
+    <div>
+    <h2>lets get started</h2>
+    <p>this is updating Tada</p>
+    </div>
+  );
+}
+
+export default App;
