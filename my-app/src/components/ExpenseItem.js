088d10b37d4febe5acd71768229cea5333a32e8a
vickythakur0398 <vickythakur0398@gmail.com>
2022-07-26 12:39:37 +0530

    38 adding date in a presentable manner using js inside app t logic comp

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -55,9 +55,17 @@ import "./ExpenseItem.css";
 // here 37- I am using props
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', {month: 'long'})
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()} </div>
+      {/* <div>{props.date.toISOString()} </div> setting date in a good presentable way 38*/}
+      <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
