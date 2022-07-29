fcfb1a5a6c7c254c2ec06123b0de3f0c8a6a8e7e
vickythakur0398 <vickythakur0398@gmail.com>
2022-07-29 11:53:07 +0530

    added conditional expense button assgnmnt

--- my-app/src/components/NewExpense/NewExpense.js
+++ my-app/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,12 @@
-import React from "react";
+
+import React, {useState} from 'react';
 
 import './NewExpense.css';
 import './ExpenseForm'
 import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
 
     const SaveExpenseDataHandler = (enteredExpenseData) =>
     {
@@ -16,8 +19,13 @@ const NewExpense = (props) => {
         console.log(expenseData);
     }
 
+    const startEditingHandler = ()=>
+    {
+        setIsEditing(true)
+    }
     return <div className="new-expense">
-        <ExpenseForm onSaveExpenseData = {SaveExpenseDataHandler}/>
+    {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+       {isEditing && <ExpenseForm onSaveExpenseData = {SaveExpenseDataHandler}/>}
     </div>
     
   
