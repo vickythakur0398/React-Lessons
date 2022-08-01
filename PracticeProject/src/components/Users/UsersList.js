2719f4ae4966c3627ed87d29e028ed9b3f5f03e7
vickythakur0398 <vickythakur0398@gmail.com>
2022-08-01 18:28:43 +0530

    imp added usersList Comp

---
+++ PracticeProject/src/components/Users/UsersList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "../UI/Card";
+
+import styles from './UsersList.module.css'
+
+const UsersList = (props) =>
+{
+        return (
+        <Card className = {styles.users}>
+        <ul>
+        {props.users.map((prop) =>(
+            <li>
+            {prop.name} ({prop.age} years old)
+            </li>
+        ))}
+       </ul>
+        
+        </Card>
+        );
+}
+
+
+export default UsersList;
