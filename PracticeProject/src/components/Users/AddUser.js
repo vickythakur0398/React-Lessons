37e35714f94bfac66d444291494da091604936a5
vickythakur0398 <vickythakur0398@gmail.com>
2022-08-01 17:57:32 +0530

    added if else logic for wrong i/p and set value for ip

--- PracticeProject/src/components/Users/AddUser.js
+++ PracticeProject/src/components/Users/AddUser.js
@@ -1,50 +1,57 @@
-import React, { useState } from 'react';
-import Card from '../UI/Card';
-import styles from './AddUser.module.css'
-import Button from '../UI/Button';
-const AddUser = (props) =>
-{
-    const [enterdUsername, setenteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+import Button from "../UI/Button";
+const AddUser = (props) => {
+  const [enteredUsername, setenteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
-    const addUserHandler =(event) =>
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+    {return;}
+    if(+enteredAge<1)
     {
-        event.preventDefault();
-        console.log(enterdUsername, enteredAge);
-        setEnteredAge('');
-        setenteredUsername('')
+        return;
     }
+    console.log(enteredUsername, enteredAge);
+    setEnteredAge("");
+    setenteredUsername("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setenteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
     
-    const usernameChangeHandler = (event) =>
-    { 
-        setenteredUsername(event.target.value);
-    }
+      setEnteredAge(event.target.value);
+    
+  };
 
-    const ageChangeHandler = (event) =>
-    { 
-        if(event.target.value <= 0)
-        { 
-            return }
-        else{
-        setEnteredAge(event.target.value);
-        }
-    }
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          value={enteredUsername}
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+        ></input>
 
+        <label htmlFor="age">Age (years)</label>
+        <input
+          value={enteredAge}
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+        ></input>
 
-    return(
-    <Card className= {styles.input}>
-    <form onSubmit={addUserHandler}>
-    <label htmlFor="username">Username</label>
-    <input id = "username" type="text" onChange={usernameChangeHandler} ></input>
-
-    <label htmlFor="age">Age (years)</label>
-    <input id = "age" type="number" onChange={ageChangeHandler} ></input>
-
-    <Button type ="submit">Add User</Button>
-    </form>
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
-    );
-}
-
+  );
+};
 
 export default AddUser;
