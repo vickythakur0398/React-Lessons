48e56ef70046109522bcd499c74fcd24fb22d9eb
vickythakur0398 <vickythakur0398@gmail.com>
2022-08-02 10:47:02 +0530

    used react fragment in app.js which is an inbuilt wrapper in react

--- PracticeProject/src/App.js
+++ PracticeProject/src/App.js
@@ -14,11 +14,11 @@ function App() {
     });
   };
   return (
-    <div>
+    <React.Fragment>
       <AddUser onAddUser={addUserHandler} />
 
       <UsersList users={usersList} />
-    </div>
+      </React.Fragment>
   );
 }
 
