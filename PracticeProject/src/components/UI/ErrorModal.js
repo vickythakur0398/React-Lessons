1d7fffd2b66233446f7a0e02e615891a21ed0f62
vickythakur0398 <vickythakur0398@gmail.com>
2022-08-02 11:49:31 +0530

    used ref and deleted state as we were only reading values on submission not updating them

--- PracticeProject/src/components/UI/ErrorModal.js
+++ PracticeProject/src/components/UI/ErrorModal.js
@@ -1,28 +1,27 @@
-import Button from './Button';
-import Card from './Card';
-import styles from './ErrorModal.module.css'
+import Button from "./Button";
+import Card from "./Card";
+import styles from "./ErrorModal.module.css";
+import React, { Fragment } from "react";
 
-const ErrorModal = (props) =>
-{
-    return (
-        <div className= {styles.backdrop} onClick={props.onConfirm}>
-        <Card className ={styles.modal}>
-            <header className={styles.header}>
+const ErrorModal = (props) => {
+  return (
+    <Fragment>
+      <div className={styles.backdrop} onClick={props.onConfirm}>
+        <Card className={styles.modal}>
+          <header className={styles.header}>
             <h2>{props.title}</h2>
-            </header>
-            <div className={styles.content}>
+          </header>
+          <div className={styles.content}>
             <p>{props.message}</p>
-            </div>
+          </div>
 
-            <footer className={styles.actions}>
+          <footer className={styles.actions}>
             <Button onClick={props.onConfirm}>Okay</Button>
-            </footer>
-
-
+          </footer>
         </Card>
-        
-        </div>
-    )
-}
+      </div>
+    </Fragment>
+  );
+};
 
 export default ErrorModal;
--- PracticeProject/src/components/Users/AddUser.js
+++ PracticeProject/src/components/Users/AddUser.js
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../UI/Wrapper";
 const AddUser = (props) => {
-  const [enteredUsername, setenteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-  // for errormodal
+ 
 
   const [error, setError] = useState();
 
+  //using ref isntaed of state
+//now after this i will add ref prop to the element i want to get value
+  const inputNameRef = useRef();
+  const inputAgeRef = useRef();
+
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    const enteredUsername = inputNameRef.current.value;
+    const enteredAge = inputAgeRef.current.value;
+    console.log(inputNameRef.current.value)
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Put valid name and age",
@@ -31,18 +38,13 @@ const AddUser = (props) => {
     }
     // console.log(enteredUsername, enteredAge);
     props.onAddUser(enteredUsername, enteredAge);
-    setEnteredAge("");
-    setenteredUsername("");
+    // here i am resetting value usimg dom but dont manipulate dom rarely do that
+    inputNameRef.current.value ='';
+    inputAgeRef.current.value ='';
+    
   };
 
-  const usernameChangeHandler = (event) => {
-    setenteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
-
+  
   //for button in errror modal
   const errorHandler = () => {
     setError(null);
@@ -63,18 +65,20 @@ const AddUser = (props) => {
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
           <input
-            value={enteredUsername}
+            
             id="username"
             type="text"
-            onChange={usernameChangeHandler}
+           
+            ref ={inputNameRef}
           ></input>
 
           <label htmlFor="age">Age (years)</label>
           <input
-            value={enteredAge}
+           
             id="age"
             type="number"
-            onChange={ageChangeHandler}
+           
+            ref = {inputAgeRef}
           ></input>
 
           <Button type="submit">Add User</Button>
