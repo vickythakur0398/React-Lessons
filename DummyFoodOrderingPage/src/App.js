9d288d0cc8b4164d558e9724d8b6e13c4be8b6ae
vickythakur0398 <vickythakur0398@gmail.com>
2022-08-03 11:51:44 +0530

    addded header and image

---
+++ DummyFoodOrderingPage/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Layout/Header';
+
+// import Header from './components/Layout/Header'
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Header></Header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ DummyFoodOrderingPage/src/components/Layout/Header.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import styles from "./Header.module.css";
+
+import FirstImage from '../../assets/FirstImage.jpg'
+
+/*This is the header section of the page whiich contains name and cart 
+and other div is for image which is present in assets and in react we can imort images 
+just like css**/
+const Header = (props) => {
+  return (
+  <Fragment >
+  <header className= {styles.header}>
+  
+  <h1>React Food Delivery</h1>
+ <button>Cart</button>
+  
+  </header>
+ {/*here i am using [] while using css bcs it has - so we cant use .*/}
+  <div className={styles['main-image']}>
+  <img src ={FirstImage} alt="Imaage full of meals"/>
+  </div>
+  </Fragment>
+)};
+
+export default Header;
